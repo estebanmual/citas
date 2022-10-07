dd98c652d0870d9667c2593ff64645c5186479aa
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-07 11:29:26 -0500

    Add button to open modal and create modal

---
+++ src/components/Formulario.js
@@ -0,0 +1,145 @@
+import React, {useState} from 'react';
+import {
+  Modal,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  TextInput,
+  View,
+  ScrollView,
+  Button,
+} from 'react-native';
+import DatePicker from 'react-native-date-picker';
+
+const Formulario = ({modalVisible}) => {
+  const [paciente, setPaciente] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [fecha, setFecha] = useState(new Date());
+  const [sintomas, setSintomas] = useState('');
+
+  const pressHandler = () => {
+    console.log('Paciente: ', paciente);
+    console.log('Propietario: ', propietario);
+    console.log('Email: ', email);
+    console.log('Telefono: ', telefono);
+    console.log('Fecha: ', fecha);
+    console.log('Sintomas: ', sintomas);
+  };
+
+  return (
+    <Modal animationType="slide" visible={modalVisible}>
+      <SafeAreaView style={styles.contenido}>
+        <ScrollView>
+          <Text style={styles.titulo}>
+            Nueva <Text style={styles.tituloBold}>Cita</Text>
+          </Text>
+          <View style={styles.campo}>
+            <Text style={styles.label}>Nombre Paciente</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nombre Paciente"
+              placeholderTextColor={'#666'}
+              value={paciente}
+              onChangeText={texto => setPaciente(texto)}
+            />
+          </View>
+          <View style={styles.campo}>
+            <Text style={styles.label}>Nombre Propietario</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nombre Propietario"
+              placeholderTextColor={'#666'}
+              value={propietario}
+              onChangeText={texto => setPropietario(texto)}
+            />
+          </View>
+          <View style={styles.campo}>
+            <Text style={styles.label}>Email Propietario</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Email Propietario"
+              placeholderTextColor={'#666'}
+              keyboardType="email-address"
+              value={email}
+              onChangeText={texto => setEmail(texto)}
+            />
+          </View>
+          <View style={styles.campo}>
+            <Text style={styles.label}>Teléfono Propietario</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Teléfono Propietario"
+              placeholderTextColor={'#666'}
+              keyboardType="number-pad"
+              value={telefono}
+              onChangeText={texto => setTelefono(texto)}
+              maxLength={10}
+            />
+          </View>
+
+          <View style={styles.campo}>
+            <Text style={styles.label}>Fecha Alta</Text>
+
+            <DatePicker date={fecha} />
+          </View>
+
+          <View style={styles.campo}>
+            <Text style={styles.label}>Síntomas</Text>
+            <TextInput
+              style={[styles.input, styles.sintomasInput]}
+              placeholder="Síntomas Paciente"
+              placeholderTextColor={'#666'}
+              value={sintomas}
+              onChangeText={texto => setSintomas(texto)}
+              multiline={true}
+              numberOfLines={4}
+            />
+          </View>
+          <View>
+            <Button title="Crear Nueva Cita" onPress={pressHandler} />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  contenido: {
+    backgroundColor: '#6D28D9',
+    flex: 1,
+  },
+  titulo: {
+    fontSize: 30,
+    fontWeight: '600',
+    textAlign: 'center',
+    marginTop: 30,
+    color: '#fff',
+  },
+  tituloBold: {
+    fontWeight: '900',
+  },
+  campo: {
+    marginTop: 10,
+    marginHorizontal: 30,
+  },
+  label: {
+    color: '#fff',
+    marginBottom: 10,
+    marginTop: 15,
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  input: {
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 10,
+  },
+  sintomasInput: {
+    height: 100,
+  },
+});
+
+export default Formulario;
